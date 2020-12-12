fa58074dd9807c1fc096e128ee99debd7ee7fcf7
Cuboctahedron <9017544+Templaar@users.noreply.github.com>
2020-12-12 22:53:33 +0100

    Create OSINT-and-websearch.c
    Part of example C code for learning purposes

---
+++ OSINT-and-websearch.c
@@ -0,0 +1,13 @@
+  var o = {};
+ o.unwatch();
+
+  void* args = alloca( args_size );
+ for( int i = 0; i < args_size; i++){
+  // Init args and my signature: #7733BUK11
+ }
+
+ if ( ((int) args[0]) & 6 == 6 )
+  args[0] = call_toString( args[0] );
+
+ if ( args_size < 1)
+  exit();
